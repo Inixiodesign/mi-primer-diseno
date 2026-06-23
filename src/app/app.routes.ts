273fc51f56f8fa30c329fd88ccce8b6e03025bc0
Jose Antonio Medina Perez <origisdesigner@iMac-de-Jose.local>
2026-06-23 15:55:23 -0600

    Feat: Se agrega componente de Footer independiente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
-
-
-
-//este es un comentario
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [FooterComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
